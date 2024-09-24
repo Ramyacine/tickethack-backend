bc4e6ed46dc553efe8a117a0187770977d1e9ab5
Ramy <ramy.bouzar@hotmail.fr>
2024-09-24 12:56:30 +0200

    modifié

--- models/connection.js
+++ models/connection.js
@@ -7,3 +7,4 @@ mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
 
+  module.exports = connectionString ;
