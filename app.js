193dd23048a5d03e16aa1a53c0016db103b85fff
Ramy <ramy.bouzar@hotmail.fr>
2024-09-24 13:08:54 +0200

    modifié

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter)
+app.use('/index', indexRouter)
 app.use('/trips', tripsRouter);
 app.use('/carts', cartsRouter);
 app.use('/booking', bookingRouter);
