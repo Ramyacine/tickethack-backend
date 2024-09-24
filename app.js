c866613eb7e43b8311b099c76f60e47162efb7ca
Ramy <ramy.bouzar@hotmail.fr>
2024-09-24 13:08:26 +0200

    modifié

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const indexRouter = require('./routes/index')
 const tripsRouter =require('./routes/trips')
 const cartsRouter = require('./routes/carts');
 const bookingRouter = require('./routes/booking');
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter)
 app.use('/trips', tripsRouter);
 app.use('/carts', cartsRouter);
 app.use('/booking', bookingRouter);
