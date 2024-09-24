d94ec6980a67ce7a53fb692a04514d8f58fd2512
Ramy <ramy.bouzar@hotmail.fr>
2024-09-24 14:17:35 +0200

    modifié

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require("dotenv").config() 
-require("./models/connection") 
+require("dotenv").config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require("./models/connection") 
 const indexRouter = require('./routes/index')
 const tripsRouter =require('./routes/trips')
 const cartsRouter = require('./routes/carts');
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter)
+app.use('/', indexRouter)
 app.use('/trips', tripsRouter);
 app.use('/carts', cartsRouter);
 app.use('/booking', bookingRouter);
